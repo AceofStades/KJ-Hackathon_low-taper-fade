18bb8e73c072bdf8537ee156ce5c5d64065080fb
AceofStades <rahulpbokade@gmail.com>
2025-02-02 05:13:40 +0530

    Minor changes

--- model/RUNMODEL.py
+++ model/RUNMODEL.py
@@ -41,3 +41,5 @@ print("Predicted churn probability:", predicted_probabilities[0])
 
 binary_prediction = int(predicted_probabilities[0] >= optimal_threshold)
 print("Predicted churn (binary):", binary_prediction)
+
+sample_df.to_csv("../modified_data.csv")
